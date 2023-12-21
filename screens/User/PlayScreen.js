040e900bd2e9ecd0918c852f3db850fa3c43fe2a
Agah Ozdemir <ozdemiragah@gmail.com>
2023-12-22 02:55:04 +0300

    activated Flip Card - goBack - nextBack buttons

--- screens/User/PlayScreen.js
+++ screens/User/PlayScreen.js
@@ -1,22 +1,54 @@
-import React, { useState } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import FlipCard from 'react-native-flip-card';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
-const RandomTextScreen = () => {
-  const [displayText, setDisplayText] = useState('Merhaba, dünyaya hoş geldiniz!');
-  const [favorites, setFavorites] = useState([]);
+const PlayPage = () => {
+  const [words, setWords] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [displayEnglish, setDisplayEnglish] = useState(true);
+  const [isFlipped, setIsFlipped] = useState(false);
 
-  const changeText = () => {
-    setDisplayText("Hello, world!");
+  useEffect(() => {
+    fetchWords();
+  }, []);
+
+  const fetchWords = async () => {
+    try {
+      const firestore = getFirestore();
+      const wordsCollection = collection(firestore, 'Words');
+      const wordsSnapshot = await getDocs(wordsCollection);
+      const wordsData = wordsSnapshot.docs.map((doc) => doc.data());
+      setWords(wordsData);
+      console.log(wordsData);
+    } catch (error) {
+      console.error('Error fetching words:', error);
+    }
   };
 
   const addToFavorites = () => {
-    setFavorites([...favorites, displayText]);
+    // Implement your logic here
+  };
+
+  const toggleDisplayLanguage = () => {
+    setIsFlipped(true);
+    setDisplayEnglish((prevDisplay) => !prevDisplay);
+    console.log('toggleDisplayLanguage');
   };
 
   const goNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex < words.length - 1 ? prevIndex + 1 : 0));
+    setIsFlipped(false);
+    setDisplayEnglish(true);
+    console.log(words[currentIndex]?.tr);
   };
 
   const goBack = () => {
+    setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : words.length - 1));
+    setIsFlipped(false);
+    setDisplayEnglish(true);
+    console.log(words[currentIndex]?.tr);
   };
 
   return (
@@ -25,20 +57,39 @@ const RandomTextScreen = () => {
         <Text style={styles.buttonText}>add to learned</Text>
       </TouchableOpacity>
 
-      <View style={styles.content}>
-        <TouchableOpacity onPress={changeText} style={styles.textContainer}>
-          <Text style={styles.text}>{displayText}</Text>
-        </TouchableOpacity>
-
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={goBack} style={styles.button}>
-            <Text style={styles.buttonText}>Back</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={goNext} style={styles.button}>
-            <Text style={styles.buttonText}>Next</Text>
+      <FlipCard
+        style={styles.cardContainer}
+        friction={2.4}
+        perspective={1000}
+        flipHorizontal={true}
+        flipVertical={false} 
+        flip={isFlipped}        // ! goBack ve goNext fonksiyonlarında kartı face (default) konumuna getirme sorunu var.
+        clickable={true}
+        onFlipEnd={(isFlipEnd) => { console.log('isFlipEnd', isFlipEnd); }}
+      >
+        {/* Front Side */}
+        <View style={[styles.card, styles.cardFront]}>
+          <TouchableOpacity onPress={toggleDisplayLanguage}>
+            <Text style={styles.cardText}>{displayEnglish ? words[currentIndex]?.eng : words[currentIndex]?.tr}</Text>
           </TouchableOpacity>
         </View>
+
+        {/* Back Side */}
+        <View style={[styles.card, styles.cardBack]}>
+          <TouchableOpacity onPress={toggleDisplayLanguage}>
+            <Text style={styles.cardText}>{displayEnglish ? words[currentIndex]?.tr : words[currentIndex]?.eng}</Text>
+          </TouchableOpacity>
+        </View>
+      </FlipCard>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={goBack} style={styles.button}>
+          <Text style={styles.buttonText}>Back</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={goNext} style={styles.button}>
+          <Text style={styles.buttonText}>Next</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -47,7 +98,8 @@ const RandomTextScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
+    justifyContent: 'center',
+    alignItems:'center',
     padding: 20,
     backgroundColor: 'white',
   },
@@ -63,24 +115,6 @@ const styles = StyleSheet.create({
     color: 'white',
     textAlign: 'center',
   },
-  content: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  textContainer: {
-    marginBottom: 50,
-    padding: 20,
-    borderRadius: 40,
-    width: '70%',
-    height: '70%',
-    justifyContent: 'center',
-    backgroundColor: '#999966',
-  },
-  text: {
-    fontSize: 18,
-    textAlign: 'center',
-  },
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -92,6 +126,31 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     width: '35%',
   },
+  cardContainer: {
+    width: 200,
+    height: 300,
+    justifyContent: 'center',
+    marginBottom: 50, 
+  },
+  card: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'lightblue',
+    borderRadius: 10,
+  },
+  cardFront: {
+    backgroundColor: 'lightblue',
+  },
+  cardBack: {
+    backgroundColor: 'lightgreen',
+  },
+  cardText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'black', // veya '#000'
+  },
 });
 
-export default RandomTextScreen;
+export default PlayPage;
