82d908a817ecad4fb58f6de94b06201a9dc4fb89
Agah Ozdemir <ozdemiragah@gmail.com>
2024-01-06 12:18:02 +0300

    fixed firebase warning

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore } from "firebase/firestore"
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyD76MI9XYjnA2HX-n_u6n7OGNxVCHtvVfs",
@@ -11,9 +13,12 @@ const firebaseConfig = {
   appId: "1:319508310654:web:b866e13cedc7e883bb462f",
   measurementId: "G-NZ1VWNGTDH"
 };
-
 const app = initializeApp(firebaseConfig);
-const auth = getAuth();
+
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
 const db = getFirestore();
 
 export {
